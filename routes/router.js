a4ae42af71ad77f18f332162250f0ab91feae6b6
fathurhdytt <fathurhidayat2003@gmail.com>
2024-06-15 11:27:39 +0700

    Add Video Section

--- routes/router.js
+++ routes/router.js
@@ -1,33 +1,34 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
 const map = express.Router();
 
-map.get('/to-login', (req, res) => {
-  res.render('login');
+map.get("/to-login", (req, res) => {
+  res.render("login");
 });
 
-
-map.get('/to-signup', (req, res) => {
-    res.render('signup');
-  });
-
-map.get('/to-aboutus', (req, res) => {
-  res.render('aboutus');
+map.get("/to-signup", (req, res) => {
+  res.render("signup");
 });
 
-map.get('/to-forgetPassword', (req, res) => {
-  res.render('forgetPassword');
+map.get("/to-aboutus", (req, res) => {
+  res.render("aboutus");
 });
 
-
-map.get('/to-profile', (req, res) => {
-  res.render('profile');
+map.get("/to-forgetPassword", (req, res) => {
+  res.render("forgetPassword");
 });
 
-map.get('/to-article', (req, res) => {
-  res.render('article');
+map.get("/to-profile", (req, res) => {
+  res.render("profile");
+});
+
+map.get("/to-article", (req, res) => {
+  res.render("article");
+});
+
+map.get("/to-video", (req, res) => {
+  res.render("video");
 });
 
 module.exports = map;
-

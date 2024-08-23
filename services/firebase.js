d1f27e1f04f823f427eb64d848d2b4419ea8a8b0
Arif <arifhida1647@gmail.com>
2024-08-23 20:51:02 +0700

    update api cron

--- services/firebase.js
+++ services/firebase.js
@@ -364,7 +364,7 @@ const deleteJobs = async (namaObat, email) => {
     });
 
     // Delete each job from the external API
-    const authToken = 'OWNOzoFyX2GvLKaGBkAToOiAgM3Q+Erz8mAxnHmTA2s=';
+    const authToken = 'cvQ1UehtttwzRbOVxWVb1YLYjlqScpmBLWO09wSqGBY=';
     for (const { jobId } of jobsToDelete) {
       const url = `https://api.cron-job.org/jobs/${jobId}`;
       const response = await fetch(url, {
@@ -446,7 +446,7 @@ const deleteJobsDetail = async (namaObat, email, jam, menit) => {
         });
 
         // Delete each job from the external API
-        const authToken = 'OWNOzoFyX2GvLKaGBkAToOiAgM3Q+Erz8mAxnHmTA2s=';
+        const authToken = 'cvQ1UehtttwzRbOVxWVb1YLYjlqScpmBLWO09wSqGBY=';
         for (const { jobId } of jobsToDelete) {
           const url = `https://api.cron-job.org/jobs/${jobId}`;
           const response = await fetch(url, {
